6212fc217b2407cff35ca700ee81756d8820f9e6
Duarte Barbosa <duartebarbosapt@gmail.com>
2025-02-05 23:52:20 +0000

    Shared Preferences can sometimes fail, so we recreate if it does.
    I think this only happens when debugging, but still, this fixes it

--- libraries/framework/src/main/java/com/scrolless/framework/core/pref/CacheManagerExtension.kt
+++ libraries/framework/src/main/java/com/scrolless/framework/core/pref/CacheManagerExtension.kt
@@ -8,6 +8,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
+import timber.log.Timber
+import javax.crypto.AEADBadTagException
 
 /**
  * @param fileName Name of the Shared Preferences
@@ -28,13 +30,35 @@ fun Context.getPrefs(fileName: String? = null): SharedPreferences {
         fileName.toString()
     }
 
-    return EncryptedSharedPreferences.create(
-        this,
-        name,
-        masterKey,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
-    )
+    return try {
+        // Attempt to create encrypted SharedPreferences
+        EncryptedSharedPreferences.create(
+            this,
+            name,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+        )
+    } catch (e: AEADBadTagException) {
+        Timber.e("Failed to create EncryptedSharedPreferences: ${e.localizedMessage}")
+
+        // Handle corrupted or incompatible SharedPreferences
+        deleteSharedPreferences(name) // Clear the corrupted data
+        EncryptedSharedPreferences.create(
+            this,
+            name,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+        )
+    } catch (e: IllegalArgumentException) { // Rethrow any unexpected exceptions
+        Timber.e(e)
+
+        throw IllegalStateException(
+            "Failed to create EncryptedSharedPreferences: ${e.localizedMessage}",
+            e,
+        )
+    }
 }
 
 /**
