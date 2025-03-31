fc9dc7cd63122c7caf93aec2e114e97faf864c23
Duarte Barbosa <duartebarbosapt@gmail.com>
2025-04-01 00:38:09 +0100

    Refactor: OnBackPressed deprecated (#45)
    * feat: Remove double back press to exit logic
    
    * feat: Navigate to HomeFragment without adding to back stack on view ready

--- app/src/main/kotlin/com/scrolless/app/features/main/MainActivity.kt
+++ app/src/main/kotlin/com/scrolless/app/features/main/MainActivity.kt
@@ -6,56 +6,23 @@ package com.scrolless.app.features.main
 
 import android.os.Bundle
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import com.scrolless.app.R
 import com.scrolless.app.base.BaseActivity
 import com.scrolless.app.databinding.ActivityMainBinding
 import com.scrolless.app.features.home.HomeFragment
-import com.scrolless.framework.extensions.showSnackBar
 import com.scrolless.framework.navigation.navigateFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-    private var backPressedOnce = false
-
     override fun onViewReady(bundle: Bundle?) {
         navigateFragment(
             HomeFragment.newInstance(),
+            addToBackStack = false,
             clearBackStack = true,
         )
 
         // Disable system insets from resizing your layout
         WindowCompat.setDecorFitsSystemWindows(window, false)
-    }
-
-    override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 1) {
-            super.onBackPressed()
-        } else {
-            if (backPressedOnce) {
-                finish()
-                return
-            }
-
-            navigateFragment(
-                HomeFragment.newInstance(),
-                clearBackStack = true,
-            )
-
-            backPressedOnce = true
-            showSnackBar(
-                binding.rootView,
-                getString(R.string.app_exit_label),
-                R.id.rootView,
-            )
-            lifecycleScope.launch {
-                delay(2000)
-                backPressedOnce = false
-            }
-        }
     }
 }
