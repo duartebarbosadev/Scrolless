628a666c8b128c209071083714c90ec277d7a222
Duarte Barbosa <duartebarbosapt@gmail.com>
2025-12-20 01:22:22 +1000

    chore: Bump version code to 12 and version name to 1.1.0

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -56,8 +56,8 @@ android {
             libs.versions.targetSdk
                 .get()
                 .toInt()
-        versionCode = 11
-        versionName = "1.0.0"
+        versionCode = 12
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
