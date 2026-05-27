9bbfc94f7bfd344cb9a42058a9e01b930b1f55c7
Duarte Barbosa <duartebarbosapt@gmail.com>
2026-05-27 04:23:10 +0100

    feat: Add shrink resources (#139)

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -118,6 +118,7 @@ android {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
