f8f276d2d088715ff0ecb373ee7d063bc8d24c3d
Duarte Barbosa <duartebarbosapt@gmail.com>
2026-09-16 20:01:52 +1000

    remove unused dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -207,7 +207,6 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
-    testImplementation(libs.mockk.android)
 
     implementation(projects.core.data)
     implementation(projects.core.designsystem)
